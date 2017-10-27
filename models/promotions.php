21b028813d7f45c0f588032c8ca06497244ef6ad
Aida93 <aidymeru18@gmail.com>
2017-10-27 11:50:12 +0200

    BootstrapCSS y añadida descLong

--- models/promotions.php
+++ models/promotions.php
@@ -25,13 +25,13 @@ if(isset($_GET['acc']) && ($_GET['acc']=='l')){
 }
 
 if(isset($_GET['acc']) && ($_GET['acc']=='s')){
-	$mySql = "SELECT p.idPromotion, p.image, date_format(p.dataExpireVals,'%d/%m/%y') as dataExpireVals, p.oferVals, p.conditionsVals, s.name FROM promotions p, shops s WHERE s.idShop = p.idShop AND p.idPromotion=".$_GET['idPromo']." ORDER BY p.dataExpireVals";
+	$mySql = "SELECT p.idPromotion, p.image, date_format(p.dataExpireVals,'%d/%m/%y') as dataExpireVals, p.oferVals, p.conditionsVals, s.name, s.telephone, s.email, s.url, s.address, s.descriptionLong, s.logo FROM promotions p, shops s WHERE s.idShop = p.idShop AND p.idPromotion=".$_GET['idPromo']." ORDER BY p.dataExpireVals";
 	$connexio = connect();
 	$resultPromotion = mysqli_query($connexio, $mySql);
 	disconnect($connexio);
 
 	$dadesPromo = mySqli_fetch_row($resultPromotion);
-	$dades='[{"image":"'.$dadesPromo[1].'", "idPromo":"'.$dadesPromo[0].'", "offer":"'.$dadesPromo[3].'", "dataExpire":"'.$dadesPromo[2].'", "conditions":"'.$dadesPromo[4].'", "nameShop":"'.$dadesPromo[5].'"}]';
+	$dades='[{"image":"'.$dadesPromo[1].'", "idPromo":"'.$dadesPromo[0].'", "offer":"'.$dadesPromo[3].'", "dataExpire":"'.$dadesPromo[2].'", "conditions":"'.$dadesPromo[4].'", "nameShop":"'.$dadesPromo[5].'", "phone":"'.$dadesPromo[6].'", "mail":"'.$dadesPromo[7].'", "url":"'.$dadesPromo[8].'", "address":"'.$dadesPromo[9].'", "descriptionLong":"'.$dadesPromo[10].'", "logo":"'.$dadesPromo[11].'"}]';
 	echo $dades;
 }
 ?>
