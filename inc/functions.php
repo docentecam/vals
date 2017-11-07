ff5e2086cd6dbbddf6b87cd8473238c69655796a
Carlos <docentecam@gmail.com>
2017-11-07 21:59:00 +0100

    Canvi idoma connexió i Missatge Cookie

--- inc/functions.php
+++ inc/functions.php
@@ -5,7 +5,7 @@
 		if (!$connexio)
 		{	die("Error al conectar");	}
 		mysqli_set_charset($connexio, "utf8");
-		mysqli_query($connexio,"SET lc_time_names = 'es_ES'");
+		mysqli_query($connexio,"SET lc_time_names = 'ca_ES'");
 		return($connexio);
 	}
 	function disconnect($connexio)
